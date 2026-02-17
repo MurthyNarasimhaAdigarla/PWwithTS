26e21020dcd0d6c0e0ddce22831ed4503a7d3fc5
murthynarasimha <murthy.adigarlaa@gmail.com>
2026-02-18 00:15:12 +0530

    pw files

---
+++ my_pw_tests/xpath_locators.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect, Locator } from '@playwright/test'
+
+test("Verify the title of the page ", async ({ page }) => {
+    await page.goto("https://demo.nopcommerce.com/");
+
+    await page.getByRole('textbox', { name: 'Search store' }).fill("shoe");
+    //await page.locator("//input[@id='small-searchterms']").fill("shoe");
+
+
+    await page.getByRole('button', { name: 'Search' }).click();
+    //await page.getByRole('img', { name: "Picture of Nike Floral Roshe Customized Running Shoes" }).click();
+
+    await page.getByAltText("Picture of Nike Floral Roshe Customized Running Shoes").click();
+    console.log(await page.locator("//span[id='price-value-26']").innerText());
+
+
+})
